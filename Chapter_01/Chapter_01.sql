b452769f96c5be7e810101f830c22489861121e0
anthonydb <adebarros@gmail.com>
2020-03-16 08:38:22 -0400

    New Chapter 1 code

---
+++ Chapter_01/Chapter_01.sql
@@ -0,0 +1,16 @@
+---------------------------------------------------------------------------
+-- Practical SQL: A Beginner's Guide to Storytelling with Data, 2nd Edition
+-- by Anthony DeBarros
+
+-- Chapter 1 Code Examples
+----------------------------------------------------------------------------
+
+-- Listing 1-1: Checking your PostgreSQL version
+
+SELECT version();
+
+
+
+
+
+
