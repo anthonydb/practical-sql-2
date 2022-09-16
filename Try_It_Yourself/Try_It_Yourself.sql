1cba023706fc4865c5a67db22cc08deec11b97aa
anthonydb <anthonymdebarros@gmail.com>
2022-09-16 09:47:20 -0400

    Minor comment spacing

--- Try_It_Yourself/Try_It_Yourself.sql
+++ Try_It_Yourself/Try_It_Yourself.sql
@@ -1,4 +1,4 @@
-    ---------------------------------------------------------------------------
+---------------------------------------------------------------------------
 -- Practical SQL: A Beginner's Guide to Storytelling with Data, 2nd Edition
 -- by Anthony DeBarros
 
