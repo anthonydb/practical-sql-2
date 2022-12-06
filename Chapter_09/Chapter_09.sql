76a9c4dcc40c13448b06ff98eb71148113a3e78f
Minhyeok <tracertw@gmail.com>
2022-12-06 12:54:45 +0000

    9장 업데이트

--- Chapter_09/Chapter_09.sql
+++ Chapter_09/Chapter_09.sql
@@ -3,7 +3,7 @@
 -- by Anthony DeBarros
 ----------------------------------------------------------------------------
 
--- 코드 9-1: Creating and filling the 2018 Public Libraries Survey table
+-- 코드 9-1: 2018년도 공공도서관 설문조사 테이블 만들고 데이터 채우기
 
 CREATE TABLE pls_fy2018_libraries (
     stabr text NOT NULL,
@@ -61,7 +61,7 @@ WITH (FORMAT CSV, HEADER);
 
 CREATE INDEX libname_2018_idx ON pls_fy2018_libraries (libname);
 
--- 코드 9-2: Creating and filling the 2017 and 2016 Public Libraries Survey tables
+-- 코드 9-2: 2016, 2017년도 공공 도서관 설문조사 테이블 생성하고 값 채우기
 
 CREATE TABLE pls_fy2017_libraries (
     stabr text NOT NULL,
@@ -175,7 +175,7 @@ CREATE INDEX libname_2017_idx ON pls_fy2017_libraries (libname);
 CREATE INDEX libname_2016_idx ON pls_fy2016_libraries (libname);
 
 
--- 코드 9-3: Using count() for table row counts
+-- 코드 9-3: count()로 테이블 행 개수 세기
 
 SELECT count(*)
 FROM pls_fy2018_libraries;
@@ -186,12 +186,12 @@ FROM pls_fy2017_libraries;
 SELECT count(*)
 FROM pls_fy2016_libraries;
 
--- 코드 9-4: Using count() for the number of values in a column
+-- 코드 9-4: count()를 이용한 NULL이 아닌 값 개수 세기
 
 SELECT count(phone)
 FROM pls_fy2018_libraries;
 
--- 코드 9-5: Using count() for the number of distinct values in a column
+-- 코드 9-5: count()를 사용해 열 안의 고유값 개수 세기
 
 SELECT count(libname)
 FROM pls_fy2018_libraries;
@@ -199,64 +199,63 @@ FROM pls_fy2018_libraries;
 SELECT count(DISTINCT libname)
 FROM pls_fy2018_libraries;
 
--- Bonus: find duplicate libnames
+-- 보너스: 같은 이름을 가진 도서관 찾기
 SELECT libname, count(libname)
 FROM pls_fy2018_libraries
 GROUP BY libname
 ORDER BY count(libname) DESC;
 
--- Bonus: see location of every Oxford Public Library
+-- 보너스: 모든 옥스포드 도서관의 위치 찾기
 SELECT libname, city, stabr
 FROM pls_fy2018_libraries
 WHERE libname = 'OXFORD PUBLIC LIBRARY';
 
--- 코드 9-6: Finding the most and fewest visits using max() and min()
+-- 코드 9-6: max()와 min()을 사용하여 최대 방문 횟수와 최소 방문 횟수 알아보기
 SELECT max(visits), min(visits)
 FROM pls_fy2018_libraries;
 
--- 코드 9-7: Using GROUP BY on the stabr column
+-- 코드 9-7: stabr 열에서 GROUP BY 사용하기
 
--- There are 55 in 2018.
+-- 2018년에는 55개가 있다.
 SELECT stabr
 FROM pls_fy2018_libraries
 GROUP BY stabr
 ORDER BY stabr;
 
--- Bonus: there are 54 in 2017.
+-- 보너스: 2017년에는 54개가 있다.
 SELECT stabr
 FROM pls_fy2017_libraries
 GROUP BY stabr
 ORDER BY stabr;
 
--- 코드 9-8: Using GROUP BY on the city and stabr columns
+-- 코드 9-8: city 열과 stabr 열에 GROUP BY 사용하기
 
 SELECT city, stabr
 FROM pls_fy2018_libraries
 GROUP BY city, stabr
 ORDER BY city, stabr;
 
--- Bonus: We can count the combos
+-- 보너스: 조합 수를 셀 수 있다.
 SELECT city, stabr, count(*)
 FROM pls_fy2018_libraries
 GROUP BY city, stabr
 ORDER BY count(*) DESC;
 
--- 코드 9-9: Using GROUP BY with count() on the stabr column
+-- 코드 9-9: 열에서 GROUP BY를 count()와 함께 사용하기
 
 SELECT stabr, count(*)
 FROM pls_fy2018_libraries
 GROUP BY stabr
 ORDER BY count(*) DESC;
 
--- 코드 9-10: Using GROUP BY with count() on the stabr and stataddr columns
+-- 코드 9-10: stabr 열과 stataddr 열에서 count()와 GROUP BY 사용
 
 SELECT stabr, stataddr, count(*)
 FROM pls_fy2018_libraries
 GROUP BY stabr, stataddr
 ORDER BY stabr, stataddr;
 
--- 코드 9-11: Using the sum() aggregate function to total visits to
--- libraries in 2016, 2017, and 2018
+-- 코드 9-11: sum() 함수를 사용하여 2018년과 2017년, 2016년 총 도서관 방문자 수 알아내기
 
 -- 2018
 SELECT sum(visits) AS visits_2018
@@ -273,7 +272,7 @@ SELECT sum(visits) AS visits_2016
 FROM pls_fy2016_libraries
 WHERE visits >= 0;
 
--- 코드 9-12: Using sum() to total visits on joined 2018, 2017, and 2016 tables
+-- 코드 9-12: sum()으로 조인한 2018, 2017, 2016년 테이블의 총 방문 수
 
 SELECT sum(pls18.visits) AS visits_2018,
        sum(pls17.visits) AS visits_2017,
@@ -285,7 +284,7 @@ WHERE pls18.visits >= 0
        AND pls17.visits >= 0
        AND pls16.visits >= 0;
 
--- Bonus: summing wifi sessions
+-- 보너스: 와이파이 세션 수 합하기
 SELECT sum(pls18.wifisess) AS wifi_2018,
        sum(pls17.wifisess) AS wifi_2017,
        sum(pls16.wifisess) AS wifi_2016
@@ -297,7 +296,7 @@ WHERE pls18.wifisess >= 0
        AND pls16.wifisess >= 0;
 
 
--- 코드 9-13: Using GROUP BY to track percent change in library visits by state
+-- 코드 9-13: GROUP BY를 사용하여 주별 도서관 방문 변화율 추적하기
 
 SELECT pls18.stabr,
        sum(pls18.visits) AS visits_2018,
@@ -316,7 +315,7 @@ WHERE pls18.visits >= 0
 GROUP BY pls18.stabr
 ORDER BY chg_2018_17 DESC;
 
--- 코드 9-14: Using HAVING to filter the results of an aggregate query
+-- 코드 9-14: HAVING 절을 사용하여 집계 함수의 결괏값을 필터링하기
 
 SELECT pls18.stabr,
        sum(pls18.visits) AS visits_2018,
