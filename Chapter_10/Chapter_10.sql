9cfa18f3917d267f9d8876978f3ff3a5dcc3e0d0
Minhyeok <tracertw@gmail.com>
2022-12-06 13:01:41 +0000

    10장 업데이트

--- Chapter_10/Chapter_10.sql
+++ Chapter_10/Chapter_10.sql
@@ -3,7 +3,7 @@
 -- by Anthony DeBarros
 ----------------------------------------------------------------------------
 
--- 코드 10-1: Importing the FSIS Meat, Poultry, and Egg Inspection Directory
+-- 코드 10-1: FSIS의 육류, 가금류 및 계란 검사 디렉터리 가져오기
 -- https://catalog.data.gov/dataset/fsis-meat-poultry-and-egg-inspection-directory-by-establishment-name
 
 CREATE TABLE meat_poultry_egg_establishments (
@@ -28,7 +28,7 @@ CREATE INDEX company_idx ON meat_poultry_egg_establishments (company);
 -- Count the rows imported:
 SELECT count(*) FROM meat_poultry_egg_establishments;
 
--- 코드 10-2: Finding multiple companies at the same address
+-- 코드 10-2: 같은 주소를 가진 여러 회사 찾기
 SELECT company,
        street,
        city,
@@ -39,14 +39,14 @@ GROUP BY company, street, city, st
 HAVING count(*) > 1
 ORDER BY company, street, city, st;
 
--- 코드 10-3: Grouping and counting states
+-- 코드 10-3: 주 그룹화하고 세기
 SELECT st, 
        count(*) AS st_count
 FROM meat_poultry_egg_establishments
 GROUP BY st
 ORDER BY st;
 
--- 코드 10-4: Using IS NULL to find missing values in the st column
+-- 코드 10-4: IS NULL을 사용하여 st 열에서 결측 값 찾기
 SELECT establishment_number,
        company,
        city,
@@ -55,7 +55,7 @@ SELECT establishment_number,
 FROM meat_poultry_egg_establishments
 WHERE st IS NULL;
 
--- 코드 10-5: Using GROUP BY and count() to find inconsistent company names
+-- 코드 10-5: GROUP BY와 count()를 사용하여 일관성 없는 회사 이름 찾아내기
 
 SELECT company,
        count(*) AS company_count
@@ -63,7 +63,7 @@ FROM meat_poultry_egg_establishments
 GROUP BY company
 ORDER BY company ASC;
 
--- 코드 10-6: Using length() and count() to test the zip column
+-- 코드 10-6: length()와 count()를 사용하여 zip 열 테스트해 보기
 
 SELECT length(zip),
        count(*) AS length_count
@@ -71,7 +71,7 @@ FROM meat_poultry_egg_establishments
 GROUP BY length(zip)
 ORDER BY length(zip) ASC;
 
--- 코드 10-7: Filtering with length() to find short zip values
+-- 코드 10-7: zip 열에서 짧은 값을 찾기 위해 length()로 필터링하기
 
 SELECT st,
        count(*) AS st_count
@@ -80,24 +80,24 @@ WHERE length(zip) < 5
 GROUP BY st
 ORDER BY st ASC;
 
--- 코드 10-8: Backing up a table
+-- 코드 10-8: 테이블 백업하기
 
 CREATE TABLE meat_poultry_egg_establishments_backup AS
 SELECT * FROM meat_poultry_egg_establishments;
 
--- Check number of records:
+-- 레코드 수 확인:
 SELECT 
     (SELECT count(*) FROM meat_poultry_egg_establishments) AS original,
     (SELECT count(*) FROM meat_poultry_egg_establishments_backup) AS backup;
 
--- 코드 10-9: Creating and filling the st_copy column with ALTER TABLE and UPDATE
+-- 코드 10-9: ALTER TABLE과 UPDATE로 st_copy 열 만들기 및 채우기
 
 ALTER TABLE meat_poultry_egg_establishments ADD COLUMN st_copy text;
 
 UPDATE meat_poultry_egg_establishments
 SET st_copy = st;
 
--- 코드 10-10: Checking values in the st and st_copy columns
+-- 코드 10-10: st 및 st_copy 열의 값 확인하기
 
 SELECT st,
        st_copy
@@ -105,7 +105,7 @@ FROM meat_poultry_egg_establishments
 WHERE st IS DISTINCT FROM st_copy
 ORDER BY st;
 
--- 코드 10-11: Updating the st column for three establishments
+-- 코드 10-11: 3개 시설에 대한 st 열 업데이트하기
 
 UPDATE meat_poultry_egg_establishments
 SET st = 'MN'
@@ -120,52 +120,52 @@ SET st = 'WI'
 WHERE establishment_number = 'M263A+P263A+V263A'
 RETURNING establishment_number, company, city, st, zip;
 
--- 코드 10-12: Restoring original st column values
+-- 코드 10-12: 원래의 st 열 값 복원하기
 
--- Restoring from the column backup
+-- 백업한 열 복원하기
 UPDATE meat_poultry_egg_establishments
 SET st = st_copy;
 
--- Restoring from the table backup
+-- 백업한 테이블 복원하기
 UPDATE meat_poultry_egg_establishments original
 SET st = backup.st
 FROM meat_poultry_egg_establishments_backup backup
 WHERE original.establishment_number = backup.establishment_number; 
 
--- 코드 10-13: Creating and filling the company_standard column
+-- 코드 10-13: company_standard 열 만들기 및 채우기
 
 ALTER TABLE meat_poultry_egg_establishments ADD COLUMN company_standard text;
 
 UPDATE meat_poultry_egg_establishments
 SET company_standard = company;
 
--- 코드 10-14: Using an UPDATE statement to modify column values that match a string
+-- 코드 10-14: UPDATE 문을 사용하여 문자열과 일치하는 필드 값 수정하기
 
 UPDATE meat_poultry_egg_establishments
 SET company_standard = 'Armour-Eckrich Meats'
 WHERE company LIKE 'Armour%'
 RETURNING company, company_standard;
 
--- 코드 10-15: Creating and filling the zip_copy column
+-- 코드 10-15: zip_copy 열 생성 및 채우기
 
 ALTER TABLE meat_poultry_egg_establishments ADD COLUMN zip_copy text;
 
 UPDATE meat_poultry_egg_establishments
 SET zip_copy = zip;
 
--- 코드 10-16: Modify codes in the zip column missing two leading zeros
+-- 코드 10-16: 두 개의 선행 0이 누락된 zip 열의 코드 수정하기
 
 UPDATE meat_poultry_egg_establishments
 SET zip = '00' || zip
 WHERE st IN('PR','VI') AND length(zip) = 3;
 
--- 코드 10-17: Modify codes in the zip column missing one leading zero
+-- 코드 10-17: 한 개의 선행 0이 누락된 zip 열의 코드 수정하기
 
 UPDATE meat_poultry_egg_establishments
 SET zip = '0' || zip
 WHERE st IN('CT','MA','ME','NH','NJ','RI','VT') AND length(zip) = 4;
 
--- 코드 10-18: Creating and filling a state_regions table
+-- 코드 10-18: state_regions 테이블 생성 및 채우기
 
 CREATE TABLE state_regions (
     st text CONSTRAINT st_key PRIMARY KEY,
@@ -176,7 +176,7 @@ COPY state_regions
 FROM 'C:\YourDirectory\state_regions.csv'
 WITH (FORMAT CSV, HEADER);
 
--- 코드 10-19: Adding and updating an inspection_deadline column
+-- 코드 10-19: inspection_deadline 열 추가 및 업데이트하기
 
 ALTER TABLE meat_poultry_egg_establishments
     ADD COLUMN inspection_deadline timestamp with time zone;
@@ -188,51 +188,51 @@ WHERE EXISTS (SELECT state_regions.region
               WHERE establishments.st = state_regions.st 
                     AND state_regions.region = 'New England');
 
--- 코드 10-20: Viewing updated inspection_deadline values
+-- 코드 10-20: 업데이트된 inspection_date 값 보기
 
 SELECT st, inspection_deadline
 FROM meat_poultry_egg_establishments
 GROUP BY st, inspection_deadline
 ORDER BY st;
 
--- 코드 10-21: Deleting rows matching an expression
+-- 코드 10-21: 표현식과 일치하는 행 삭제하기
 
 DELETE FROM meat_poultry_egg_establishments
 WHERE st IN('AS','GU','MP','PR','VI');
 
--- 코드 10-22: Removing a column from a table using DROP
+-- 코드 10-22: DROP을 사용하여 테이블에서 열 제거하기
 
 ALTER TABLE meat_poultry_egg_establishments DROP COLUMN zip_copy;
 
--- 코드 10-23: Removing a table from a database using DROP
+-- 코드 10-23: DROP을 사용하여 데이터베이스에서 테이블 제거하기
 
 DROP TABLE meat_poultry_egg_establishments_backup;
 
--- 코드 10-24: Demonstrating a transaction block
+-- 코드 10-24: 트랜잭션 블록 수행하기
 
--- Start transaction and perform update
+-- 트랜잭션을 시작하고 업데이트하기
 START TRANSACTION;
 
 UPDATE meat_poultry_egg_establishments
 SET company = 'AGRO Merchantss Oakland LLC'
 WHERE company = 'AGRO Merchants Oakland, LLC';
 
--- view changes
+-- 변경사항 확인하기
 SELECT company
 FROM meat_poultry_egg_establishments
 WHERE company LIKE 'AGRO%'
 ORDER BY company;
 
--- Revert changes
+-- 변경사항 무시하기
 ROLLBACK;
 
--- See restored state
+-- 복구 내용 확인하기
 SELECT company
 FROM meat_poultry_egg_establishments
 WHERE company LIKE 'AGRO%'
 ORDER BY company;
 
--- Alternately, commit changes at the end:
+-- 변경 사항을 적용하려면 마지막에 COMMIT을 입력
 START TRANSACTION;
 
 UPDATE meat_poultry_egg_establishments
@@ -241,14 +241,14 @@ WHERE company = 'AGRO Merchants Oakland, LLC';
 
 COMMIT;
 
--- 코드 10-25: Backing up a table while adding and filling a new column
+-- 코드 10-25: 새 열을 추가하고 채우면서 테이블 백업하기
 
 CREATE TABLE meat_poultry_egg_establishments_backup AS
 SELECT *,
        '2023-02-14 00:00 EST'::timestamp with time zone AS reviewed_date
 FROM meat_poultry_egg_establishments;
 
--- 코드 10-26: Swapping table names using ALTER TABLE
+-- 코드 10-26: ALTER TABLE을 사용하여 테이블 이름 바꾸기
 
 ALTER TABLE meat_poultry_egg_establishments 
     RENAME TO meat_poultry_egg_establishments_temp;
