ddc453a3f65e4762ed444b787f339fbc102fc953
Minhyeok <tracertw@gmail.com>
2022-12-06 11:49:26 +0000

    3장 수정

--- Chapter_03/Chapter_03.sql
+++ Chapter_03/Chapter_03.sql
@@ -3,7 +3,7 @@
 -- by Anthony DeBarros
 ----------------------------------------------------------------------------
 
--- 코드 3-1: Querying all rows and columns from the teachers table
+-- 코드 3-1: teachers 테이블의 모든 행과 열 조회하기
 
 SELECT * FROM teachers;
 
@@ -11,71 +11,70 @@ SELECT * FROM teachers;
 
 TABLE teachers;
 
--- 코드 3-2: Querying a subset of columns
+-- 코드 3-2: 열의 하위 집합 쿼리하기
 
 SELECT last_name, first_name, salary FROM teachers;
 
--- 코드 3-3: Sorting a column with ORDER BY
+-- 코드 3-3: ORDER BY로 열 정렬하기
 
 SELECT first_name, last_name, salary
 FROM teachers
 ORDER BY salary DESC;
 
--- Note you can also specify the sort column by
--- using a number representing its position in the result.
+-- 열 이름 대신 숫자도 입력할 수 있습니다. 정렬 기준으로 삼을 열을 지정할 때
+-- 이름 대신 SELECT 절에서 해당 열이 반환되는 위치를 넣으면 됩니다.
 
 SELECT first_name, last_name, salary
 FROM teachers
 ORDER BY 3 DESC;
 
--- 코드 3-4: Sorting multiple columns with ORDER BY
+-- 코드 3-4: ORDER BY를 사용해서 여러 열 정렬하기
 
 SELECT last_name, school, hire_date
 FROM teachers
 ORDER BY school ASC, hire_date DESC;
 
--- 코드 3-5: Querying distinct values in the school column
+-- 코드 3-5: school 열에서 별개의 값을 쿼리하기
 
 SELECT DISTINCT school
 FROM teachers
 ORDER BY school;
 
--- 코드 3-6: Querying distinct pairs of values in the school and salary
--- columns
+-- 코드 3-6: school과 salary 열의 고유한 데이터 쌍 쿼리하기
 
 SELECT DISTINCT school, salary
 FROM teachers
 ORDER BY school, salary;
 
--- 코드 3-7: Filtering rows using WHERE
+-- 코드 3-7: WHERE를 사용하여 행 필터링하기
 
 SELECT last_name, school, hire_date
 FROM teachers
 WHERE school = 'Myers Middle School';
 
--- Examples of WHERE comparison operators
+-- WHERE와 비교 연산자 사용하기
 
--- Teachers with first name of Janet
+-- Janet 이름을 가진 선생님 찾기
 SELECT first_name, last_name, school
 FROM teachers
 WHERE first_name = 'Janet';
 
--- School names not equal to F.D. Roosevelt HS
+-- F.D. Roosevelt HS를 제외한 모든 학교 이름 출력하기
 SELECT school
 FROM teachers
 WHERE school <> 'F.D. Roosevelt HS';
 
--- Teachers hired before Jan. 1, 2000
+-- 2000년 1월 1일 이전에 고용된 선생님 출력하기
 SELECT first_name, last_name, hire_date
 FROM teachers
 WHERE hire_date < '2000-01-01';
 
--- Teachers earning 43,500 or more
+-- 연봉이 $43,500 이상인 선생님 찾기
 SELECT first_name, last_name, salary
 FROM teachers
 WHERE salary >= 43500;
 
--- Teachers who earn from $40,000 to $65,000
+-- 연봉이 $40,000~$65,000인 선생님 찾기
 SELECT first_name, last_name, school, salary
 FROM teachers
 WHERE salary BETWEEN 40000 AND 65000;
@@ -84,7 +83,7 @@ SELECT first_name, last_name, school, salary
 FROM teachers
 WHERE salary >= 40000 AND salary <= 65000;
 
--- 코드 3-8: Filtering with LIKE AND ILIKE
+-- 코드 3-8: LIKE와 ILIKE로 필터링하기
 
 SELECT first_name
 FROM teachers
@@ -94,7 +93,7 @@ SELECT first_name
 FROM teachers
 WHERE first_name ILIKE 'sam%';
 
--- 코드 3-9: Combining operators using AND and OR
+-- 코드 3-9: AND와 OR를 사용해 조건 결합하기
 
 SELECT *
 FROM teachers
@@ -111,16 +110,15 @@ FROM teachers
 WHERE school = 'F.D. Roosevelt HS'
       AND (salary < 38000 OR salary > 40000);
 
--- Note how the results change if we omit parentheses. That's
--- because the AND operator takes precedence over OR and is
--- evaluated first:
+-- 괄호를 생략할 경우 결과가 어떻게 변화하는지 확인하세요.
+-- AND 연산자를 OR 보다 먼저 평가합니다.
 
 SELECT *
 FROM teachers
 WHERE school = 'F.D. Roosevelt HS'
       AND salary < 38000 OR salary > 40000;
 
--- 코드 3-10: A SELECT statement including WHERE and ORDER BY
+-- 코드 3-10: WHERE과 ORDER BY를 포함한 SELECT 문
 
 SELECT first_name, last_name, school, hire_date, salary
 FROM teachers
